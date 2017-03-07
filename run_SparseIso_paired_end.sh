03da07d904f65fe14d9e8845e00f4d5b0cb9d9df
henryxushi <henryxushi@gmail.com>
2017-03-07 01:05:45 -0500

    V1.0

--- run_SparseIso_paired_end.sh
+++ run_SparseIso_paired_end.sh
@@ -1,3 +1,4 @@
+PATH_BAMTOOLS_LIB=
 BAMFILE=/home/henry/SparseIso_V1.0/demo/demo_paired_end.bam
 SPARSEISO_PATH=/home/henry/SparseIso_V1.0
 OUTPUT=/home/henry/SparseIso_V1.0/demo/demo_paired_end
@@ -7,5 +8,5 @@ chmod 777 $SPARSEISO_PATH/SparseIso
 chmod 777 $SPARSEISO_PATH/tools/samtools
 chmod 777 $SPARSEISO_PATH/tools/processsam
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SPARSEISO_PATH/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PATH_BAMTOOLS_LIB
 $SPARSEISO_PATH/SparseIso -b $BAMFILE -o $OUTPUT -r p -p $NUM_OF_PROCESSES --cempath $SPARSEISO_PATH/tools --sampath $SPARSEISO_PATH/tools
