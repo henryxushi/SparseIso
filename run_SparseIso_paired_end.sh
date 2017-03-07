c10b8223e4f986e0c120be2839e57b27c16723cd
henryxushi <henryxushi@gmail.com>
2017-03-06 22:47:40 -0500

    V1.0

---
+++ run_SparseIso_paired_end.sh
@@ -0,0 +1,11 @@
+BAMFILE=/home/henry/SparseIso_V1.0/demo/demo_paired_end.bam
+SPARSEISO_PATH=/home/henry/SparseIso_V1.0
+OUTPUT=/home/henry/SparseIso_V1.0/demo/demo_paired_end
+NUM_OF_PROCESSES=5
+
+chmod 777 $SPARSEISO_PATH/SparseIso
+chmod 777 $SPARSEISO_PATH/tools/samtools
+chmod 777 $SPARSEISO_PATH/tools/processsam
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SPARSEISO_PATH/lib
+$SPARSEISO_PATH/SparseIso -b $BAMFILE -o $OUTPUT -r p -p $NUM_OF_PROCESSES --cempath $SPARSEISO_PATH/tools --sampath $SPARSEISO_PATH/tools
---
+++ run_SparseIso_single_end.sh
@@ -0,0 +1,12 @@
+BAMFILE=/home/henry/SparseIso_V1.0/demo/demo_single_end.bam
+SPARSEISO_PATH=/home/henry/SparseIso_V1.0
+OUTPUT=/home/henry/SparseIso_V1.0/demo/demo_single_end
+NUM_OF_PROCESSES=5
+
+chmod 777 $SPARSEISO_PATH/SparseIso
+chmod 777 $SPARSEISO_PATH/tools/samtools
+chmod 777 $SPARSEISO_PATH/tools/processsam
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SPARSEISO_PATH/lib
+$SPARSEISO_PATH/SparseIso -b $BAMFILE -o $OUTPUT -r s -p $NUM_OF_PROCESSES --cempath $SPARSEISO_PATH/tools --sampath $SPARSEISO_PATH/tools
+
