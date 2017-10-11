be2a9f8218a7c690eefe0e6982938ef78abe136b
henryxushi <henryxushi@gmail.com>
2017-10-11 12:07:57 -0400

    V1.1
    add support for STAR and HISAT2

--- run_SparseIso_paired_end.sh
+++ run_SparseIso_paired_end.sh
@@ -6,7 +6,7 @@ NUM_OF_PROCESSES=5
 
 chmod 777 $SPARSEISO_PATH/SparseIso
 chmod 777 $SPARSEISO_PATH/tools/samtools
-chmod 777 $SPARSEISO_PATH/tools/processsam
+chmod 777 $SPARSEISO_PATH/tools/processsamS
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PATH_BAMTOOLS_LIB
 $SPARSEISO_PATH/SparseIso -b $BAMFILE -o $OUTPUT -r p -p $NUM_OF_PROCESSES --cempath $SPARSEISO_PATH/tools --sampath $SPARSEISO_PATH/tools
